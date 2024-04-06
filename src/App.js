4113922681a90270bb6da212445740f49317f4e2
whitebird1016 <takus.superdev@gmail.com>
2024-04-06 20:55:14 +0800

    add loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import LoadImg from "./assets/load.gif";
+import LogoImg from "./assets/logo.png";
 
 const webWidth = window.innerWidth;
 const webHeight = window.innerHeight;
@@ -11,15 +13,40 @@ const webglHeight =
   webWidth / webHeight > 1920 / 890 ? webHeight : (webWidth * 890) / 1920;
 
 const App = () => {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, loadingProgression } = useUnityContext({
     loaderUrl: "build/WebGL.loader.js",
     dataUrl: "build/WebGL.data",
     frameworkUrl: "build/WebGL.framework.js",
     codeUrl: "build/WebGL.wasm",
   });
 
+  const outputConsole = () => {
+    const progress = Math.floor(loadingProgression * 100);
+    switch (progress) {
+      case 1:
+        console.log("Just Started");
+        break;
+      case 50:
+        console.log("Please take your time");
+        break;
+      case 90:
+        console.log("Almost Done");
+        break;
+
+      default:
+        break;
+    }
+  }
+  outputConsole()
   return (
     <WebGLWrapper>
+      {loadingProgression < 1 && (
+        <LoadWrapper>
+          <Logo src={LogoImg} alt="logo" />
+          <Load src={LoadImg} alt="Loading..." />
+        </LoadWrapper>
+      )}
+
       <WebGLContainer>
         <WebGLProvider unityProvider={unityProvider} />
       </WebGLContainer>
@@ -31,6 +58,7 @@ const WebGLWrapper = styled.div`
   width: 100%;
   height: 100vh;
   overflow: hidden;
+  background:black;
 `;
 const WebGLContainer = styled.div`
   display: flex;
@@ -45,4 +73,16 @@ const WebGLProvider = styled(Unity)`
   height: ${webglHeight}px;
   
 `;
+const LoadWrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  height:100%
+`
+const Load = styled.img`
+`
+const Logo = styled.img`
+  width:350px;
+`
 export default App;
